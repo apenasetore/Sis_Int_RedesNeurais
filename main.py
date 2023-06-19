dc3bedb3685f92e1be0fd0e24e93bb15dc1e0620
apenasetore <63430172+apenasetore@users.noreply.github.com>
2023-06-19 16:03:51 -0300

    tá classificando

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split    
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 
 #processando os dados
 m = dataprocess.data_txt_to_matrix()
@@ -20,14 +20,21 @@ y_test = Label.fit_transform(y_test)
 
 scaler = StandardScaler()
 scaler.fit(x_train)
+scaler.fit(x_test)
 
+#print(len(x_test))
+#print(len(x_train))
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
-MLP = MLPRegressor(hidden_layer_sizes=(10,10,10), max_iter=1000)
-MLP.fit(x_train,y_train)
+#print(len(x_test))
+#print((x_train))
+
+
+MLP = MLPClassifier(hidden_layer_sizes=(10,10,10), max_iter=1000)
+MLP.fit(x_train,y_train.ravel())
 
 predictions = MLP.predict(x_test)
-print(y_test)
-print(predictions)
-#print(confusion_matrix(y_test,predictions))
-#print(classification_report(y_test,predictions))
+#print(len(y_test))
+#print(len(predictions))
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
